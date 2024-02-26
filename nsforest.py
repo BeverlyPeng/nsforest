9d4dc6dcd9d5a47df5e9f090e4fd02835ff94a25
Beverly Peng <42978187+BeverlyPeng@users.noreply.github.com>
2024-02-26 15:18:15 -0800

    Update nsforest.py

--- nsforest.py
+++ nsforest.py
@@ -1,15 +1,6 @@
 
 ### Libraries ###
-import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.tree import DecisionTreeClassifier
-import time
-from tqdm import tqdm
-import os
-import myrandomforest
-import mydecisiontreeevaluation
-# import utils
-# import psutil
+
 
 # v4.0 includes new parameter, "gene_selection," which determines whether BinaryFirst is used or not and its cutoff value
 def NSForest(variable):
