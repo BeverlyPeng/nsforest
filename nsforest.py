a6ff50d8e8d20af1cc35cc0965866bcd80e401aa
Beverly Peng <42978187+BeverlyPeng@users.noreply.github.com>
2024-02-27 12:47:50 -0800

    Create nsforest.py

---
+++ nsforest.py
@@ -0,0 +1,15 @@
+
+import pandas as pd
+
+def NSForest(variable = "Hello"):
+    
+  """
+  Performs NSForest algorithm to find an optimal list of marker genes. 
+  
+  :param variable: a string
+  :type variable: str
+  :return: a list of string
+  :rtype: list[str]
+  """
+
+  return ["a", "list", "of", "string"]
